e49a813c39724432fb3fd6615e89de93f65ac2f0
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-04-01 14:04:26 +0300

    demo comment

--- Demo.cpp
+++ Demo.cpp
@@ -1,7 +1,8 @@
 /**
- * Demo program for Gimatriya calculation.
+ * Demo program for phonetic search.
  * 
- * Date: 2020-02
+ * Author: Erel Segal-Halevi
+ * Since : 2020-03
  */
 
 #include "PhoneticFinder.hpp"
